6ea388d15873cb875fb0af2025ba3147165de6cf
Morgan <Rabelais@C103-8>
2018-11-26 16:36:57 +0100

    AjoutModele

---
+++ src/Modele/Salle.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modele;
+
+/**
+ *
+ * @author Rabelais
+ */
+public class Salle {
+      private String refsalle,surface,typerevement;
+
+    public String getRefsalle() {
+        return refsalle;
+    }
+
+    public void setRefsalle(String refsalle) {
+        this.refsalle = refsalle;
+    }
+
+    public String getSurface() {
+        return surface;
+    }
+
+    public void setSurface(String surface) {
+        this.surface = surface;
+    }
+
+    public String getTyperevement() {
+        return typerevement;
+    }
+
+    public void setTyperevement(String typerevement) {
+        this.typerevement = typerevement;
+    }
+      
+}

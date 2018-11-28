579791a5cb8f51e1800b611f29848ec489721c0c
morga <morga@LAPTOP-N5D6142D>
2018-11-28 20:56:49 +0100

    Ajoutsportv1

--- src/Controlleur/AjoutSportController.java
+++ src/Controlleur/AjoutSportController.java
@@ -5,17 +5,28 @@
  */
 package Controlleur;
 
+import Modele.Gestionsql;
+import Modele.Sport;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+import javafx.scene.control.Dialogs;
 /**
  * FXML Controller class
  *
  * @author Rabelais
  */
 public class AjoutSportController implements Initializable {
-
+     private Stage dialogstage;
+            @FXML
+    private TextField txtSport;
+    private   Sport sport=new Sport();
+    private  Gestionsql gestions=new Gestionsql();
+    
+        private boolean okClick=false;
     /**
      * Initializes the controller class.
      */
@@ -23,5 +34,34 @@ public class AjoutSportController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
-    
+        public boolean isOkclick()
+    {
+       return  okClick;
+    }
+    private void handleOk()
+    {
+      if(isInputValid())
+      {
+          sport.setNomSport(txtSport.getText());
+          
+          okClick=true;
+          dialogstage.close();
+      }
+    }
+ 
+
+    private boolean isInputValid() {
+        String messageErreur="";
+        boolean retour=true;
+    if(txtSport.getText()==null || txtSport.getText().length()== 0)
+    {
+        messageErreur="Sport invalide";
+    }
+    if(messageErreur.length()>0)
+    {
+      Dialogs.showErrorDialog(dialogstage,messageErreur,"SVP corrigez le champ invalide","champ invalide");
+    retour=false;
+    }
+ return  retour;
+    }
 }
